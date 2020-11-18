007662a4b5a515730108b7cbf85daae64a9779d0
Sebastian Obara <obara.sebastian@gmail.com>
2020-11-18 01:05:28 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='homo',
+    version='2020.11.18.1',
+    description='Homo',
+    author='Sebastian Obara',
+    author_email='obara.sebastian@gmail.com',
+    package_data={},
+    dependency_links=[
+
+    ],
+    install_requires=[
+        'requests == 2.25.0',
+        'tldextract == 3.0.2'
+        'idna == 2.10',
+        'pysocks == 1.7.1'
+    ],
+)
